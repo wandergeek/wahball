c79014187b0f0c1decb7df45d0eca66739665278
nick clark <nick.clark@identitii.com>
2019-12-27 23:05:18 +1100

    clean up, add debug modes - not yet tested

--- wahball-arduino.ino
+++ wahball-arduino.ino
@@ -6,46 +6,91 @@
 #include <FastLED.h>
 #include <SPI.h>
 
+boolean DEBUG_MSA = false;
+boolean DEBUG_MAP = true;
+
+Adafruit_MSA301 msa;
 byte address = 0x00;
-byte POT1 = 10;
-byte POT2 = 9;
-byte POT3 = 8;
+byte EXP_A_PIN = 10;
+byte EXP_B_PIN = 9;
+byte EXP_C_PIN = 8;
 
 #define NUM_LEDS 5
 #define DATA_PIN 9
 CRGB leds[NUM_LEDS];
 
-Adafruit_MSA301 msa;
-
 byte expression_a_val = 0;
-byte expression_b_val = 128;
-byte expression_c_val = 255;
+byte expression_b_val = 0;
+byte expression_c_val = 0;
 
 void setup(void) {
   Serial.begin(115200);
-  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-  FastLED.setBrightness(  64 );
 
-  pinMode (POT1, OUTPUT);
-  pinMode (POT2, OUTPUT);
-  pinMode (POT3, OUTPUT);
-  
+  pinMode(EXP_A_PIN, OUTPUT);
+  pinMode(EXP_B_PIN, OUTPUT);
+  pinMode(EXP_C_PIN, OUTPUT);
   SPI.begin();
-
-  return(0);
   
-  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
-
-  Serial.println("Adafruit MSA301 test!");
+  while (!Serial) delay(10);
   
-  // Try to initialize!
   if (! msa.begin()) {
     Serial.println("Failed to find MSA301 chip");
     while (1) { delay(10); }
   }
-  Serial.println("MSA301 Found!");
+  Serial.println("MSA301 Found.");
+  if(DEBUG_MSA == true) printMSASettings();
 
-  //msa.setDataRate(MSA301_DATARATE_31_25_HZ);
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.setBrightness(  64 );
+  setStripColor(CRGB::Green);
+  delay(500);
+  setStripColor(CRGB::Black);
+}
+
+void loop() {
+  msa.read();
+  if(DEBUG_MSA == true) {
+    Serial.print("X:  "); Serial.print(msa.x); 
+    Serial.print("  \tY:  "); Serial.print(msa.y); 
+    Serial.print("  \tZ:  "); Serial.print(msa.z); 
+  }
+  
+  byte hue = map(msa.x, -2000,2000,0, 255);
+  byte saturation = map(msa.y, -2000,2000,0, 255);
+  setStripColor(CHSV(hue,255,100));
+  
+  byte expression_a_val = map(msa.x, -2000,2000,0, 255);
+  byte expression_b_val = map(msa.y, -2000,2000,0, 255);
+  byte expression_c_val = map(msa.z, -2000,2000,0, 255);
+
+  if(DEBUG_MAP == true) {
+    Serial.print("A:"); Serial.print(expression_a_val);
+    Serial.print("\tB:"); Serial.print(expression_b_val);
+    Serial.print("\tC:"); Serial.println(expression_c_val);
+    delay(100);
+  }
+
+  digitalPotWrite(expression_a_val,EXP_A_PIN);
+  digitalPotWrite(expression_b_val,EXP_B_PIN);
+  digitalPotWrite(expression_c_val,EXP_C_PIN);
+}
+
+void setStripColor(CRGB c){
+  for(int i=0; i<NUM_LEDS; i++){
+    leds[i] = c;
+  }
+  FastLED.show();
+}
+
+int digitalPotWrite(byte value, byte pin) {
+  digitalWrite(pin, LOW);
+  SPI.transfer(address);
+  SPI.transfer(value);
+  digitalWrite(pin, HIGH);
+}
+
+ void printMSASettings() {
+ //msa.setDataRate(MSA301_DATARATE_31_25_HZ);
   Serial.print("Data rate set to: ");
   switch (msa.getDataRate()) {
     case MSA301_DATARATE_1_HZ: Serial.println("1 Hz"); break;
@@ -100,52 +145,4 @@ void setup(void) {
     case MSA301_RESOLUTION_10: Serial.println("10 bits"); break;
     case MSA301_RESOLUTION_8: Serial.println("8 bits"); break;
   }
-
-  setStripColor(CRGB::Green);
-  delay(500);
-  setStripColor(CRGB::Black);
-}
-
-void loop() {
-  msa.read();      // get X Y and Z data at once
-  // Then print out the raw data
-  Serial.print("X:  "); Serial.print(msa.x); 
-  Serial.print("  \tY:  "); Serial.print(msa.y); 
-  Serial.print("  \tZ:  "); Serial.print(msa.z); 
-  
-  
-  byte hue = map(msa.x, -2000,2000,0, 255);
-  byte saturation = map(msa.y, -2000,2000,0, 255);
-  setStripColor(CHSV(hue,255,100));
-  
-  // byte expression_a_val = map(msa.x, -2000,2000,0, 255);
-  expression_a_val++;
-  expression_b_val++;
-  expression_c_val++;
-//  byte expression_b_val = map(msa.y, -2000,2000,0, 255);
-
-   digitalPotWrite(expression_a_val,POT1);
-   digitalPotWrite(expression_b_val,POT2);
-   digitalPotWrite(expression_c_val,POT3);
-
-  Serial.print("  \tA out:  "); Serial.print(expression_a_val); 
-//  Serial.print("  \tB out:  "); Serial.print(expression_b_val); 
-  
-  Serial.println();
-  delay(100); 
-}
-
-void setStripColor(CRGB c){
-  for(int i=0; i<NUM_LEDS; i++){
-    leds[i] = c;
-  }
-  FastLED.show();
-}
-
-int digitalPotWrite(byte value, byte pin)
-{
-digitalWrite(pin, LOW);
-SPI.transfer(address);
-SPI.transfer(value);
-digitalWrite(pin, HIGH);
 }
