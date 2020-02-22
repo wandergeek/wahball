05b6d7b6a50b0fe91a22c52a26a3a4173218b35b
nick clark <nick.clark@identitii.com>
2020-02-22 15:34:48 +1100

    mode switching working

--- wahball.ino
+++ wahball.ino
@@ -3,6 +3,12 @@
 #include "leds.h"
 #include "pots.h"
 
+#define MODE_SELECT_PIN 3
+#define MODE_WAHBALL 1
+#define MODE_OSCILLATOR 2
+#define NUM_MODES 2 //I hate this, but itll do for now
+byte curMode;
+
 byte expression_values[] = {0,0,0};
 boolean DEBUG_MAP = false;
 
@@ -17,24 +23,33 @@ void setup(void) {
   accel.setup();
   leds.setup();
   pots.setup();
+
+  pinMode(MODE_SELECT_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(MODE_SELECT_PIN), advanceMode, CHANGE);
+  curMode = MODE_WAHBALL;
 }
 
 void loop() {
   accel.update();
   pots.update();
-
-  byte hue = map(accel.x(), -2000,2000,0, 255);
-  leds.setAll(CHSV(hue,255,100));
   
-  expression_values[0] = map(accel.x(), -2000,2000,0, 255);
-  expression_values[1] = map(accel.y(), -2000,2000,0, 255);
-  expression_values[2] = map(accel.z(), -2000,2000,0, 255);
 
-  if(DEBUG_MAP == true) {
-    printExpressionValues();
+  if(curMode == MODE_WAHBALL) {
+    byte hue = map(accel.x(), -2000,2000,0, 255);
+    leds.setAll(CHSV(hue,255,100));
+    expression_values[0] = map(accel.x(), -2000,2000,0, 255);
+    expression_values[1] = map(accel.y(), -2000,2000,0, 255);
+    expression_values[2] = map(accel.z(), -2000,2000,0, 255);
+    if(DEBUG_MAP == true) { printExpressionValues(); }  
+    expression.update(expression_values);
+
+  } else if(curMode == MODE_OSCILLATOR) {
+    Serial.println("woo im oscillating!");
+    delay(100);
   }
 
-  expression.update(expression_values);
+
+  
 }
 
 void printExpressionValues() {
@@ -47,4 +62,10 @@ void printExpressionValues() {
     } 
 }
 
-
+void advanceMode() {
+    curMode++;
+    if(curMode > NUM_MODES) {
+        curMode = 1;
+    }
+    Serial.print("switching to mode ");Serial.println(curMode);
+}
